8081328cdddbc59741d4c5625219ad7df229005d
ljy <jyoppang@naver.com>
2025-05-10 04:41:30 +0900

    c

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { auth, db } from '../firebase';
 import { useNavigate } from 'react-router-dom';
