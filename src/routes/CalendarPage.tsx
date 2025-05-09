8c6462d5430f248a414b54ddc87ce6dc67249b17
ljy <jyoppang@naver.com>
2025-05-10 05:23:10 +0900

    d

--- src/routes/CalendarPage.tsx
+++ src/routes/CalendarPage.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Calendar from 'react-calendar';
 import type { CalendarProps } from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import styled from 'styled-components';
